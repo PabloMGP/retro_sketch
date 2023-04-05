ed06f840b316e1d42fe2ed6b3e09743ab58baecb
Pablo <pgarciacardone@gmail.com>
2023-04-05 14:14:15 +0100

    Add coloring functionality
    Add flag variable isMouseDown for tracking mouse state
    Add mouse down, over, up eventListeners to work with paint func
    Set userSelect to none to prevent mouse down from selecting squares

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+});
+
+let isMouseDown = false;
+
 // Get the grid container element
-let gridContainer = document.getElementById('grid-container');
+let gridContainer = document.querySelector('.grid-container');
 
 // Function to generate grid of squares
 function generateGrid(rows, cols) {
@@ -7,13 +12,29 @@ function generateGrid(rows, cols) {
   gridContainer.style.gridTemplateColumns = `repeat(${cols}, ${squareSize}%)`;
   gridContainer.style.gridTemplateRows = `repeat(${rows}, ${squareSize}%)`;
   gridContainer.innerHTML = ''; // Clear existing squares
-
   for (let i = 0; i < rows * cols; i++) {
     let square = document.createElement('div');
     square.classList.add('square');
     gridContainer.appendChild(square);
+    square.style.userSelect = 'none';  // Prevents mouse(while down) from selecting the divs
   }
-}
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.addEventListener('mousedown', function () {
+      isMouseDown = true;
+      paint(square);
+    });
+    square.addEventListener("mouseover", function () {
+      if (isMouseDown === true) {
+        paint(square);
+      }
+    });
+    square.addEventListener("mouseup", function () {
+      isMouseDown = false;
+    })
+  })
+};
+
 
 // Call the function with desired rows and columns
 let rows = 40; // Adjust number of rows as needed
@@ -21,12 +42,17 @@ let cols = 40; // Adjust number of columns as needed
 generateGrid(rows, cols);
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const scaleUp = document.querySelector(".button1");
-    scaleUp.addEventListener("click", () => {
-      let rows = 10;
-      let cols = 10;
-      generateGrid(rows, cols); // Call the generateGrid function with the updated rows and cols values
-    });
+
+  const scaleUp = document.querySelector(".button1");
+  scaleUp.addEventListener("click", () => {
+    let rows = 10;
+    let cols = 10;
+    generateGrid(rows, cols); // Call the generateGrid function with the updated rows and cols values
   });
-  
+
+
+// Primary paint function
+function paint(square) {
+  square.style.backgroundColor = "blue";
+}
+
